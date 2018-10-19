872304ae5e0eaad496ecc1baa7c580d8b9e1e08f
André Moriya <aheidi.moriya@gmail.com>
2018-10-19 09:23:08 -0300

    Change service

--- src/main/java/io/icodetech/service/CepService.java
+++ src/main/java/io/icodetech/service/CepService.java
@@ -1,22 +1,37 @@
 package io.icodetech.service;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.google.common.collect.Maps;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class CepService {
+	
+	private static final Map<String, String> response = Maps.newHashMap();
 
 	@HystrixCommand(fallbackMethod = "findBy")
 	public String findByCep(String cep) {
 		RestTemplate client = new RestTemplate();
-		String response = client.getForObject(String.format("https://viacep.com.br/ws/%s/json", cep), String.class);
+		String res = client.getForObject(String.format("https://viacep.com.br/ws/%s/json", cep), String.class);
+		response.put(cep, res);
 		
-		return response;
+		return res;
 	}
 	
 	public String findBy(String cep) {
-		return cep;
+		String result = "";
+		Optional<String> value = Optional.ofNullable(response.get(cep));
+		if(value.isPresent()) {
+			result = value.get();
+			return result;
+		} else {
+			result = "{\"status\": \"404\", \"messagem\": \"CEP não encontrado\"}";
+		}
+		return result;
 	}
 }
